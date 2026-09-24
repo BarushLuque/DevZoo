2dff419df8bffe37947082548394023f06ee7bdf
BarushLuque <barushcamargo@gmail.com>
2026-09-24 11:55:15 -0700

    hola

---
+++ app.py
@@ -0,0 +1,13 @@
+from PIL import Image
+
+# Cambia 'tu_imagen.jpg' por la ruta de tu archivo de imagen
+ruta_imagen = 'imagen\a.png'
+
+try:
+    # Abrir la imagen
+    imagen = Image.open(ruta_imagen)
+    
+    # Mostrar la imagen en el visor predeterminado del sistema
+    imagen.show()
+except FileNotFoundError:
+    print(f"No se encontró la imagen en la ruta: {ruta_imagen}")
